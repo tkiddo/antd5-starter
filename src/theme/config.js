b2dcf0a713294bd569ee931ba3d24f0ed89b143d
tkiddo <justforgit@163.com>
2022-12-07 15:28:04 +0800

    feat: theme config

--- src/theme/config.js
+++ src/theme/config.js
@@ -1,16 +1,30 @@
-
-
-const baseToken={
+const baseToken = {
   colorPrimary: '#fa541c',
+  colorBgSpotlight: '#00b96b',
 }
 
 const radioToken = {
-  colorPrimary:'#00b96b'
+  colorPrimary: '#00b96b',
+}
+
+const modalToken = {
+  borderRadiusLG: 20,
+}
+
+const tooltipToken = {
+  colorBgSpotlight: '#00b96b',
+}
+
+const buttonToken={
+  borderRadius:0,
 }
 
 export default {
-  token:baseToken,
-  components:{
-    Radio:radioToken
-  }
+  token: baseToken,
+  components: {
+    Radio: radioToken,
+    Modal: modalToken,
+    Tooltip: tooltipToken,
+    Button:buttonToken
+  },
 }
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import { Button, Radio } from 'antd'
+import React, { useState } from 'react'
+import { Button, Radio, Checkbox, Modal, Tooltip } from 'antd'
 
 const Home = () => {
+  const [modalOpen, setModalOpen] = useState(false)
   return (
     <>
-      <Button type="primary">primary button</Button>
+      <Button type="primary" onClick={() => setModalOpen(true)}>
+        primary button
+      </Button>
       <Radio.Group>
-        <Radio value={1}>A</Radio>
+        <Tooltip title="提示">
+          <Radio value={1}>A</Radio>
+        </Tooltip>
       </Radio.Group>
+      <Checkbox>Checkbox</Checkbox>
+      <Modal
+        title="modal"
+        open={modalOpen}
+        onCancel={() => setModalOpen(false)}
+      >
+        content
+      </Modal>
     </>
   )
 }
